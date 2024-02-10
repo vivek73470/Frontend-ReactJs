72f1b3e1b1ed645e07a4acd4f5ea3e604484ce4c
vivek <vivek734706@gmail.com>
2024-02-10 12:11:51 +0530

    deleted estra file

--- Books/src/Pages/Books/Books.jsx
+++ Books/src/Pages/Books/Books.jsx
@@ -16,25 +16,3 @@ export const Books = () => {
         </div>
     )
 }
-
-const BookPageWrapper = styled.div`
-    display:flex;
-    border:1px solid red;
-
-`;
-
-const FilterWrapper = styled.div`
-    width:300px;
-    border:1px solid red
-`;
-
-
-const BookWrapper = styled.div`
-    width:100%;
-    border:5px solid green;
-    display:grid;
-    grid-template-columns: 100px 100px 100px;
-    //grid-template-columns:repeat(auto-fit, minmax(100px,max-content))
-    grid-gap:150px;
-    justify-content:center
-`
