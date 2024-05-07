fadff92a75c855c1951bdeb37d62ede1d56a7785
vivek <vivek734706@gmail.com>
2024-05-08 01:18:51 +0530

    remove loader

--- E-Commerce/src/Pages/Products/Products.jsx
+++ E-Commerce/src/Pages/Products/Products.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
+import React, { } from "react";
 import './products.css'
 import { Filter } from "../../Components/FilterComponent/FilterComponent"
 import {useDispatch, useSelector} from "react-redux"
 import {useEffect} from "react" 
 import {useSearchParams} from "react-router-dom"
 import {fetchData} from "../../Redux/products/action";
-import loader from '../../../src/Assets/load.jpg'
 import Footer from "../../Components/Footer/footer";
 
 
 const Products = () => {
-    const[loading,setLoading] = useState(true)
   const dispatch=useDispatch()
   const watches=useSelector((store)=>store.ProductReducer.products)
 
@@ -18,15 +16,11 @@ const Products = () => {
 
 
   useEffect(() => {
-    setLoading(true);
     let getParams = {
       params: { category: searchParams.getAll("category") }
     }
-
-    setTimeout(() => { 
       dispatch(fetchData(getParams))
-      setLoading(false);
-    }, 100); 
+
   }, [searchParams, dispatch])
 
 
@@ -38,14 +32,7 @@ const Products = () => {
             <Filter />
           </div>
           <div className='product-listing'>
-            {loading ? ( 
-                <div className="loaderimgg">
-                    <img src = {loader} alt="prd"/>
-                      <p>Loading...</p>
-                </div>
-          
-            ) : (
-              watches.length > 0 &&
+            {watches.length > 0 &&
               watches.map((item) => (
                 <div className="productlist-design" key={item.id}>
                   <img className='product-imgstyle' src={item.image} alt="cloth products" />
@@ -59,8 +46,7 @@ const Products = () => {
                             <p className='product-title-members'>₹{item.discount_price_box} For Tribe Members</p>
                 </div>
               ))
-              
-            )}
+               }
           </div>
         </div>
       </div>
