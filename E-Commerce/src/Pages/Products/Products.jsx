08b36754d9ff8410ce8f81c5d305e76cb3f9549c
vivek <vivek734706@gmail.com>
2024-05-08 00:36:08 +0530

    work on db

--- E-Commerce/src/Pages/Products/Products.jsx
+++ E-Commerce/src/Pages/Products/Products.jsx
@@ -40,7 +40,7 @@ const Products = () => {
           <div className='product-listing'>
             {loading ? ( 
                 <div className="loaderimgg">
-                    <img src = {loader}/>
+                    <img src = {loader} alt="prd"/>
                       <p>Loading...</p>
                 </div>
           
@@ -48,9 +48,15 @@ const Products = () => {
               watches.length > 0 &&
               watches.map((item) => (
                 <div className="productlist-design" key={item.id}>
-                  <img className='product-imgstyle' src={item.image} alt="prodct" />
+                  <img className='product-imgstyle' src={item.image} alt="cloth products" />
+                  <p className='product-brandname'>{item.brand_namez}</p>
                   <p className='product-actual-title'>{item.title}</p>
-                  <p className='product-actual-price'>₹{item.price}</p>
+                  {/* <p className='product-actual-price'>₹{item.price}</p> */}
+                  <div className='product-price-description'>
+                                <p className='product-discount-price'>₹{item.discountedPriceText}</p>
+                                <p className='product-actual-price'>₹{item.actualPriceText}</p>
+                            </div>
+                            <p className='product-title-members'>₹{item.discount_price_box} For Tribe Members</p>
                 </div>
               ))
               
